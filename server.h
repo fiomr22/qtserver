a597dd29c5615f3d7b770d477da6319c2d2cbe47
Fiorella Meza <fiorellamr.22@gmail.com>
2021-10-23 20:44:30 -0500

    Proyecto Servidor inicial

---
+++ server.h
@@ -0,0 +1,29 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QWidget>
+#include <QTcpServer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Server; }
+QT_END_NAMESPACE
+
+class Server : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Server(QWidget *parent = nullptr);
+    ~Server();
+private:
+    void initServer();
+private slots:
+    void sendRandomMessage();
+private:
+    Ui::Server *ui;
+    //Socket servidor
+    QTcpServer *serverSocket = nullptr;
+    //mensajes
+    QVector <QString> randomMessages;
+};
+#endif // SERVER_H
